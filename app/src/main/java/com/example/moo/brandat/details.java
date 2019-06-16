349dfc601a05fbb59c7fdc12557e6829cdf99524
Bohlok <mahmodb29@gmail.com>
2019-06-16 20:21:59 +0200

    mahmoud

--- app/src/main/java/com/example/moo/brandat/details.java
+++ app/src/main/java/com/example/moo/brandat/details.java
@@ -1,44 +1,64 @@
 package com.example.moo.brandat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Picasso;
 
+import java.util.Collections;
+
 public class details extends AppCompatActivity {
-    TextView fname;TextView category;TextView casee;TextView cost;TextView pdescribe;TextView location;TextView ownername;TextView phone;TextView email;
-ImageView imageView;
-CircularImageView circularImageView;
+    TextView fname;
+    TextView category;
+    TextView casee;
+    TextView cost;
+    TextView pdescribe;
+    TextView location;
+    TextView ownername;
+    TextView phone;
+    TextView email;
+    ImageView imageView;
+    CircularImageView circularImageView;
+    private FusedLocationProviderClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        fname=(TextView)findViewById(R.id.fullName);
-        category=(TextView)findViewById(R.id.category);
-        casee=(TextView)findViewById(R.id.case2);
-        cost=(TextView)findViewById(R.id.cost);
-        ownername=(TextView)findViewById(R.id.ownerName);
-        pdescribe=(TextView)findViewById(R.id.descripe);
-        location=(TextView)findViewById(R.id.location);
-        phone=(TextView)findViewById(R.id.phone);
-        email=(TextView)findViewById(R.id.email);
-imageView=findViewById(R.id.viewImage);
-circularImageView=findViewById(R.id.circularImageView);
+        fname = (TextView) findViewById(R.id.fullName);
+        category = (TextView) findViewById(R.id.category);
+        casee = (TextView) findViewById(R.id.case2);
+        cost = (TextView) findViewById(R.id.cost);
+        ownername = (TextView) findViewById(R.id.ownerName);
+        pdescribe = (TextView) findViewById(R.id.descripe);
+        location = (TextView) findViewById(R.id.location);
+        phone = (TextView) findViewById(R.id.phone);
+        email = (TextView) findViewById(R.id.email);
+        imageView = findViewById(R.id.viewImage);
+        circularImageView = findViewById(R.id.circularImageView);
 
 
-
-
-
-        Intent intent = getIntent();
-       String name = intent.getStringExtra("fname");
+        final Intent intent = getIntent();
+        String name = intent.getStringExtra("fname");
         String cat = intent.getStringExtra("category");
         String cos = intent.getStringExtra("cost");
         String em = intent.getStringExtra("email");
@@ -50,7 +70,7 @@ circularImageView=findViewById(R.id.circularImageView);
         final String uImg = intent.getStringExtra("img_url");
         final String uId = intent.getStringExtra("user_id");
 
-        Log.d("imgitem", "onImageClick: "+uImg);
+        Log.d("imgitem", "onImageClick: " + uImg);
 
         fname.setText(name);
         category.setText(cat);
@@ -62,24 +82,120 @@ circularImageView=findViewById(R.id.circularImageView);
         phone.setText(fon);
 
         fname.setText(name);
+        client = LocationServices.getFusedLocationProviderClient(details.this);
+        requestPermission();
         phone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View w) {
-                Intent intent2=new Intent(details.this, map_auto.class);
-                startActivity(intent2);
+
+                final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    Intent intent3 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent3);
+                    if (ActivityCompat.checkSelfPermission(details.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    client.getLastLocation().addOnSuccessListener(details.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location !=null){
+                                double l= location.getLatitude();
+                                double g=location.getLongitude();
+                                String ls=""+l;
+                                String gs=""+g;
+                                Toast.makeText(details.this, ls, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(details.this, gs, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+
+
+                }
+else {
+                    if (ActivityCompat.checkSelfPermission(details.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    client.getLastLocation().addOnSuccessListener(details.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                double l = location.getLongitude();
+                                double g = location.getLatitude();
+                                String ls = "" + l;
+                                String gs =  ""+ g;
+                                Toast.makeText(details.this, ls, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(details.this, gs, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+
+
+//                    Intent intent2=new Intent(details.this, map_auto.class);
+//                    startActivity(intent2);
+
+                }
+                if (ActivityCompat.checkSelfPermission(details.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                   //  public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                 //                                             int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(details.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location !=null){
+                            double l= location.getLatitude();
+                            double g=location.getLongitude();
+                            String ls=""+l;
+                            String gs=""+g;
+
+                            Toast.makeText(details.this, ls, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(details.this, gs, Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
+
 
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View w) {
                 Intent intent1=new Intent(details.this, Main_map.class);
-                double l=50.00351;
-                double g= 50.053748;
+                double l=30.00351;
+                double g= 30.053748;
                 String ls=""+l;
                 String gs=""+g;
                 intent1.putExtra("long", ls);
                 intent1.putExtra("lat", gs);
+                startActivity(intent1);
+            }
+        });
+        category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View w) {
+                Intent intent1=new Intent(details.this, map_auto.class);
                 startActivity(intent1);
             }
         });
@@ -101,6 +217,11 @@ circularImageView=findViewById(R.id.circularImageView);
 
 
 
+
+
+    }
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
 
     }
--- app/src/main/java/com/example/moo/brandat/map_auto.java
+++ app/src/main/java/com/example/moo/brandat/map_auto.java
@@ -1,66 +1,201 @@
 package com.example.moo.brandat;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import android.util.Log;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
+//import com.mapbox.mapboxandroiddemo.R;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-public class map_auto extends Activity implements LocationListener {
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
-    protected Context context;
-    TextView txtLat;
-    String lat;
-    String provider;
-    protected String latitude, longitude;
-    protected boolean gps_enabled, network_enabled;
+import static com.example.moo.brandat.R.drawable.mapbox_marker_icon_default;
 
+/**
+ * Animate the marker to a new position on the map.
+ */
+public class map_auto extends AppCompatActivity implements OnMapReadyCallback,
+        MapboxMap.OnMapClickListener {
+
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private LatLng currentPosition = new LatLng(64.900932, -18.167040);
+    private GeoJsonSource geoJsonSource;
+    private ValueAnimator animator;
+    double l;
+    double g;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Mapbox access token is configured here. This needs to be called either in your application
+        // object or in the same activity which contains the mapview.
+        Mapbox.getInstance(this,"pk.eyJ1IjoibWFobW91ZGJvaGxvayIsImEiOiJjanV6dWs5ZXEwdXE3NDRteDZ2dzVxaHpuIn0.bqGTQQYwO4KjTq3ANr0HVg");
+
+        // This contains the MapView in XML and needs to be called after the access token is configured.
         setContentView(R.layout.activity_map_auto);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+Button save_map=(Button) findViewById(R.id.button5);
+        save_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View w) {
+
+
+                String ls=""+l;
+                String gs=""+g;
+                Toast.makeText(map_auto.this, gs, Toast.LENGTH_SHORT).show();
+                Toast.makeText(map_auto.this, ls, Toast.LENGTH_SHORT).show();          }
+
+        });
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+
+        geoJsonSource = new GeoJsonSource("source-id",
+                Feature.fromGeometry(Point.fromLngLat(currentPosition.getLongitude(),
+                        currentPosition.getLatitude())));
+
+
+        mapboxMap.setStyle(Style.SATELLITE_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+
+                style.addImage(("marker_icon"), BitmapFactory.decodeResource(
+                        getResources(), mapbox_marker_icon_default));
+
+                style.addSource(geoJsonSource);
+
+                style.addLayer(new SymbolLayer("layer-id", "source-id")
+                        .withProperties(
+                                PropertyFactory.iconImage("marker_icon"),
+                                PropertyFactory.iconIgnorePlacement(true),
+                                PropertyFactory.iconAllowOverlap(true)
+                        ));
+
+
+                mapboxMap.addOnMapClickListener(map_auto.this);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        // When the user clicks on the map, we want to animate the marker to that
+        // location.
+        if (animator != null && animator.isStarted()) {
+            currentPosition = (LatLng) animator.getAnimatedValue();
+            animator.cancel();
+            l=currentPosition.getLatitude();
+            g=currentPosition.getLongitude();
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-    }
-    @Override
-    public void onLocationChanged(Location location) {
-        int c=0;
-                if (c<=1) {
-                    txtLat = (TextView) findViewById(R.id.textview1);
-                    txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-              c++;  }}
 
-    @Override
-    public void onProviderDisabled(String provider) {
-        Log.d("Latitude","disable");
-    }
+        animator = ObjectAnimator
+                .ofObject(latLngEvaluator, currentPosition, point)
+                .setDuration(2000);
+        animator.addUpdateListener(animatorUpdateListener);
+        animator.start();
 
-    @Override
-    public void onProviderEnabled(String provider) {
-        Log.d("Latitude","enable");
+        currentPosition = point;
+        l=currentPosition.getLatitude();
+         g=currentPosition.getLongitude();
+
+        return true;
     }
 
+    private final ValueAnimator.AnimatorUpdateListener animatorUpdateListener =
+            new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    LatLng animatedPosition = (LatLng) valueAnimator.getAnimatedValue();
+                    geoJsonSource.setGeoJson(Point.fromLngLat(animatedPosition.getLongitude(), animatedPosition.getLatitude()));
+                }
+            };
+
+    // Class is used to interpolate the marker animation.
+    private static final TypeEvaluator<LatLng> latLngEvaluator = new TypeEvaluator<LatLng>() {
+
+        private final LatLng latLng = new LatLng();
+
+        @Override
+        public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
+            latLng.setLatitude(startValue.getLatitude()
+                    + ((endValue.getLatitude() - startValue.getLatitude()) * fraction));
+            latLng.setLongitude(startValue.getLongitude()
+                    + ((endValue.getLongitude() - startValue.getLongitude()) * fraction));
+            return latLng;
+        }
+    };
+
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude","status");
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (animator != null) {
+            animator.cancel();
+        }
+        if (mapboxMap != null) {
+            mapboxMap.removeOnMapClickListener(this);
+        }
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
